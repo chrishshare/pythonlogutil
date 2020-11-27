c0bcc1e404c7fefbc6c49b658221c6d44180a301
chrishshare <chrishshare@163.com>
2020-11-28 07:49:14 +0800

    解决打包后安装时，不同的包以目录的形式安装问题

--- setup.py
+++ setup.py
@@ -6,30 +6,9 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-__author__ = 'chrishshare <chrishshare@163.com>'
-
-
-with open('README.md') as readme_file:
-    long_description = readme_file.read()
-
-
-# 'setup.py publish' shortcut.
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
-    sys.exit()
-elif sys.argv[-1] == 'clean':
-    import shutil
-    if os.path.isdir('build'):
-        shutil.rmtree('build')
-    if os.path.isdir('dist'):
-        shutil.rmtree('dist')
-    if os.path.isdir('chromedriver_autoinstaller.egg-info'):
-        shutil.rmtree('chromedriver_autoinstaller.egg-info')
-
-
 setup(
     name="pythonlog",
+    include_package_data=True,
     version="0.0.1",
     author="chrishshare",
     author_email="chrishshare@163.com",
@@ -37,7 +16,8 @@ setup(
     license="MIT",
     keywords="log logs logutil",
     url="https://github.com/yeongbin-jo/python-chromedriver-autoinstaller",
-    packages=find_packages(where='pythonlog', include=['*']),
-    package_dir={"": "pythonlog"},
-    data_files=[('./config/logger.json', ['pythonlog/config/logger.json'])]
+    packages=find_packages(),  # include all packages under src
+    # package_dir={"": "pythonlog"},
+    package_data={"pythonlog": ["config/logger.json"]}
+    # data_files=[('./config/logger.json', ['pythonlog/config/logger.json'])]
 )
